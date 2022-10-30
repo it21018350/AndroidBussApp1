9827e06251bf2466874317aca1c369588cb16fdf
SMUDU <sajithlakshan199@gmail.com>
2022-10-30 21:19:54 +0530

    Add Vibration

--- app/src/main/java/com/example/androidbussapp1/android_large___21_activity.java
+++ app/src/main/java/com/example/androidbussapp1/android_large___21_activity.java
@@ -7,9 +7,12 @@ import android.app.Activity;
 import android.os.Bundle;
 
 
+import android.os.Vibrator;
 import android.view.View;
-import android.widget.CompoundButton;
+import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class android_large___21_activity extends Activity {
 
@@ -34,14 +37,34 @@ public class android_large___21_activity extends Activity {
 
 		
 		_bg__android_large___21_ek2 = (View) findViewById(R.id._bg__android_large___21_ek2);
-		rectangle_4_ek21 = (View) findViewById(R.id.rectangle_4_ek21);
+
 		rectangle_1_ek36 = (View) findViewById(R.id.rectangle_1_ek36);
 		sound = (TextView) findViewById(R.id.sound);
 		reminder_settings = (TextView) findViewById(R.id.reminder_settings);
 		rectangle_11_ek5 = (View) findViewById(R.id.rectangle_11_ek5);
-		vibrate = (TextView) findViewById(R.id.vibrate);
+	//	vibrate = (TextView) findViewById(R.id.vibrate);
 
-		TextView aswitch = findViewById(R.id.VON);
+		final Button aswitch = findViewById(R.id.VON);
+		final Vibrator vibr = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+		final long[] pattern = {2000,1000};
+		aswitch.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if(aswitch.getText().toString().equals("Start")){
+					vibr.vibrate(pattern,0);
+					Toast.makeText(android_large___21_activity.this,"Started",Toast.LENGTH_SHORT).show();
+					aswitch.setText("STOP");
+
+
+
+				}else {
+					vibr.cancel();
+					aswitch.setText("Start");
+					Toast.makeText(android_large___21_activity.this,"Canseled",Toast.LENGTH_SHORT).show();
+
+				}
+			}
+		});
 
 
 
